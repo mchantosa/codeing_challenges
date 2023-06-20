a8ab48cd3a83239e64a05a37e3fb9d67299a3c81
Megan Chantosa <meganemmamoore@gmail.com>
2023-06-20 16:01:05 -0700

    fibonacci improved

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -1,13 +1,34 @@
-function fibonacci(n) {
+function fibonacciBUMemoize(n) {
+  //1, 1, 2, 3, 5, 8, 13, 21
+  const fibs = [0, 1];
+
+  function searchFibs(i, limit) {
+    //base cases
+    if (i > limit) return;
+
+    //processing
+    fibs[i] = fibs[i - 1] + fibs[i - 2];
+
+    //recursive cases
+    searchFibs(i + 1, limit);
+  }
+
+  searchFibs(2, n);
+
+  //recursive case
+  return fibs[n];
+}
+
+function fibonacciTD(n) {
   //1, 1, 2, 3, 5, 8, 13, 21
   //base case
   if (n === 0) return 0;
   if (n === 1) return 1;
   //recursive case
-  else return fibonacci(n - 2) + fibonacci(n - 1);
+  else return fibonacciTD(n - 2) + fibonacciTD(n - 1);
 }
 
-function fibonacciMemoize(n, tracking = []) {
+function fibonacciTDMemoize(n, tracking = []) {
   //0, 1, 1, 2, 3, 5, 8, 13, 21
 
   //base case
@@ -17,14 +38,15 @@ function fibonacciMemoize(n, tracking = []) {
   //recursive case
   if (!tracking[n]) {
     tracking[n] =
-      fibonacciMemoize(n - 2, tracking) + fibonacciMemoize(n - 1, tracking);
+      fibonacciTDMemoize(n - 2, tracking) + fibonacciTDMemoize(n - 1, tracking);
   }
 
   return tracking[n];
 }
 
-console.log(fibonacci(1) === 1);
-console.log(fibonacci(2) === 1);
-console.log(fibonacci(3) === 2);
-console.log(fibonacci(8) === 21);
-console.log(fibonacciMemoize(60));
+console.log(fibonacciTD(1) === 1);
+console.log(fibonacciTD(2) === 1);
+console.log(fibonacciTD(3) === 2);
+console.log(fibonacciTD(8) === 21);
+console.log(fibonacciBUMemoize(60));
+console.log(fibonacciTDMemoize(60));
