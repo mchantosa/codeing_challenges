148c40599b73d2438c5612f62e99167905afc8e0
mchantosa <meganemmamoore@gmail.com>
2023-07-26 21:03:40 -0700

    haven't comitted in awhile, not sure what all changes I have

---
+++ dynamic_programming/knapsack_problem.js
@@ -0,0 +1,14 @@
+/**
+ *  0/1 Knapsack Problem
+ *  Items, weights, and values are given. Find the maximum value that can be obtained.
+ *  You are a thief carrying a single knapsack with limited weight capacity (10lb).
+ * 
+ *              Item | Weight | Value
+ *              1    | 2      | 3
+ *              2    | 2      | 7
+ *              3    | 4      | 2
+ *              4    | 5      | 9
+ *  
+ *  
+ * 
+ */
